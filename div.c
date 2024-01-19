92af5fe260b9f269d999fcde95be310e6dc16d8e
KeaMohlala <keamogetsemhll@gmail.com>
2024-01-19 07:57:31 -0800

    implement di opcode

---
+++ div.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+
+/**
+* op_div - divides top element from second top element of the stack
+* @stack: pointer to head pointer of the stack
+* @line_number: file line number
+*/
+
+void op_div(stack_t **stack, unsigned int line_number)
+{
+stack_t *top, *second_top;
+
+if (*stack == NULL || (*stack)->next == NULL)
+{
+fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+return;
+}
+
+if (top->n == 0)
+{
+fprintf(stderr, "L%d: division by zero\n", line_number);
+return;
+}
+
+top = *stack;
+second_top = top->next;
+
+second_top->n /= top->n;
+if (top->prev != NULL)
+{
+top->prev->next = top->next;
+}
+if (top->next != NULL)
+{
+top->next->prev = top->prev;
+}
+
+free(top);
+*stack = second_top;
+}
